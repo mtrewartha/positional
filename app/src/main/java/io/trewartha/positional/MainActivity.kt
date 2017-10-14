516582ea2838c923a2bc78ab1968bf952fb95ba4
Mike Trewartha <mike@gospotcheck.com>
2017-10-14 16:26:52 -0600

    Add sign in and profile activities

--- app/src/main/java/io/trewartha/positional/MainActivity.kt
+++ app/src/main/java/io/trewartha/positional/MainActivity.kt
@@ -1,26 +1,38 @@
 package io.trewartha.positional
 
+import android.app.Activity
+import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.app.AppCompatDelegate
 import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import io.trewartha.positional.common.GlideApp
+import io.trewartha.positional.common.TextDrawable
 import kotlinx.android.synthetic.main.main_activity.*
+
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
+        private const val REQUEST_CODE_SIGN_IN = 1
+
         init {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO)
         }
     }
 
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
     private lateinit var fragmentPagerAdapter: MainFragmentPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.main_activity)
 
         fragmentPagerAdapter = MainFragmentPagerAdapter(supportFragmentManager)
@@ -28,12 +40,51 @@ class MainActivity : AppCompatActivity() {
         viewPager.addOnPageChangeListener(PageChangeListener())
 
         bottomNavigationView.setOnNavigationItemSelectedListener(NavigationItemSelectedListener())
+
+        userPhotoImageView.setOnClickListener { onUserPhotoClick() }
+        loadUserPhoto()
+
+        if (!signedIn()) {
+            val signInIntent = Intent(this, SignInActivity::class.java)
+            startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN)
+        }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        fragmentPagerAdapter.notifyDataSetChanged()
+        when (requestCode) {
+            REQUEST_CODE_SIGN_IN -> if (resultCode != Activity.RESULT_OK) finish()
+            else -> fragmentPagerAdapter.notifyDataSetChanged()
+        }
     }
+
+    private fun loadUserPhoto() {
+        val url = firebaseAuth.currentUser?.photoUrl
+        val unknownDrawable = TextDrawable(
+                this,
+                "?",
+                ContextCompat.getColor(this, R.color.gray3),
+                ContextCompat.getColor(this, R.color.white)
+        )
+        GlideApp.with(this)
+                .load(url)
+                .placeholder(unknownDrawable)
+                .error(unknownDrawable)
+                .circleCrop()
+                .into(userPhotoImageView)
+    }
+
+    private fun onUserPhotoClick() {
+        val profileIntent = ProfileActivity.IntentBuilder(this).build()
+        val options = ActivityOptions.makeSceneTransitionAnimation(
+                this,
+                userPhotoImageView,
+                getString(R.string.transition_user_photo)
+        ).toBundle()
+        startActivity(profileIntent, options)
+    }
+
+    private fun signedIn() = firebaseAuth.currentUser != null
 
     private inner class NavigationItemSelectedListener : BottomNavigationView.OnNavigationItemSelectedListener {
         override fun onNavigationItemSelected(item: MenuItem): Boolean {
---
+++ app/src/main/java/io/trewartha/positional/common/GlideModule.kt
@@ -0,0 +1,8 @@
+package io.trewartha.positional.common
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule
+class GlideModule : AppGlideModule()
