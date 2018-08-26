741f24c7137a4e3200d0ed8e0edc9f970467ca8b
Mike Trewartha <trewartha.mike@gmail.com>
2018-08-26 16:52:04 -0600

    Use a red onSurface color at night

--- app/src/main/java/io/trewartha/positional/ui/MainActivity.kt
+++ app/src/main/java/io/trewartha/positional/ui/MainActivity.kt
@@ -1,7 +1,10 @@
 package io.trewartha.positional.ui
 
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.trewartha.positional.R
@@ -12,6 +15,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        if (currentNightMode == UI_MODE_NIGHT_YES) window.decorView.systemUiVisibility =
+                SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                SYSTEM_UI_FLAG_FULLSCREEN or
+                SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
         setContentView(R.layout.main_activity)
 
