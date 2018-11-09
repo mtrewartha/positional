678403614329b821a5e0808f980176c9bbdeb848
Mike Trewartha <trewartha.mike@gmail.com>
2018-11-08 21:48:48 -0700

    Add a link to the privacy policy to Settings

--- app/src/main/java/io/trewartha/positional/ui/settings/SettingsFragment.kt
+++ app/src/main/java/io/trewartha/positional/ui/settings/SettingsFragment.kt
@@ -1,7 +1,11 @@
 package io.trewartha.positional.ui.settings
 
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import io.trewartha.positional.R
 
@@ -16,5 +20,24 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.settings, rootKey)
     }
 
+    override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+        return if (preference?.key == getString(R.string.settings_privacy_policy_key)) {
+            showPrivacyPolicy()
+            true
+        } else {
+            super.onPreferenceTreeClick(preference)
+        }
+    }
+
     override fun getCallbackFragment(): Fragment = this
+
+    private fun showPrivacyPolicy() {
+        val intent = Intent(ACTION_VIEW, Uri.parse(PRIVACY_POLICY_URL))
+        startActivity(intent)
+    }
+
+    companion object {
+        private const val PRIVACY_POLICY_URL =
+            "https://github.com/miketrewartha/positional/blob/master/PRIVACY.md"
+    }
 }
