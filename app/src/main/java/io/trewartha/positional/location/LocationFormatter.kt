c3fc0d1cb0f7fe96a3aa9cd654e092e6e56e9ad7
Mike Trewartha <trewartha.mike@gmail.com>
2018-11-19 21:43:26 -0700

    Use lat bands for UTM coords and consider speed to be empty if zero

--- app/src/main/java/io/trewartha/positional/location/LocationFormatter.kt
+++ app/src/main/java/io/trewartha/positional/location/LocationFormatter.kt
@@ -22,10 +22,6 @@ class LocationFormatter(context: Context) {
     private val formatSpeedMetric by lazy { context.getString(R.string.location_speed_metric) }
     private val locationValueUnknown by lazy { context.getString(R.string.location_value_unknown) }
 
-    companion object {
-        private const val HEMISPHERE_NORTH = "gov.nasa.worldwind.avkey.North"
-    }
-
     fun getAccuracy(
         location: Location?,
         metric: Boolean
@@ -103,12 +99,6 @@ class LocationFormatter(context: Context) {
         return MGRSCoord.fromLatLon(latAngle, longAngle).toString()
     }
 
-    fun getUtmZone(latitude: Double, longitude: Double): String {
-        val utmCoordinate = getUtmCoordinate(latitude, longitude)
-        val hemisphere = if (HEMISPHERE_NORTH == utmCoordinate.hemisphere) "N" else "S"
-        return "${utmCoordinate.zone}$hemisphere "
-    }
-
     fun getUtmEasting(latitude: Double, longitude: Double): String {
         val utmCoordinate = getUtmCoordinate(latitude, longitude)
         return "${String.format(
@@ -116,6 +106,30 @@ class LocationFormatter(context: Context) {
             formatCoordinateUtm,
             utmCoordinate.easting
         )}m E"
+    }
+
+    fun getUtmLatitudeBand(latitude: Double): String = when {
+        -80.0 <= latitude && latitude < -72.0 -> "C"
+        -72.0 <= latitude && latitude < -64.0 -> "D"
+        -72.0 <= latitude && latitude < -56.0 -> "E"
+        -72.0 <= latitude && latitude < -48.0 -> "F"
+        -72.0 <= latitude && latitude < -40.0 -> "G"
+        -72.0 <= latitude && latitude < -32.0 -> "H"
+        -72.0 <= latitude && latitude < -24.0 -> "J"
+        -72.0 <= latitude && latitude < -16.0 -> "K"
+        -72.0 <= latitude && latitude < -8.0 -> "L"
+        -72.0 <= latitude && latitude < 0.0 -> "M"
+        00.0 <= latitude && latitude < 08.0 -> "N"
+        08.0 <= latitude && latitude < 16.0 -> "P"
+        16.0 <= latitude && latitude < 24.0 -> "Q"
+        24.0 <= latitude && latitude < 32.0 -> "R"
+        32.0 <= latitude && latitude < 40.0 -> "S"
+        40.0 <= latitude && latitude < 48.0 -> "T"
+        48.0 <= latitude && latitude < 56.0 -> "U"
+        56.0 <= latitude && latitude < 64.0 -> "V"
+        64.0 <= latitude && latitude < 72.0 -> "W"
+        72.0 <= latitude && latitude < 84.0 -> "X"
+        else -> ""
     }
 
     fun getUtmNorthing(latitude: Double, longitude: Double): String {
@@ -127,45 +141,48 @@ class LocationFormatter(context: Context) {
         )}m N"
     }
 
-    private fun getUtmCoordinate(latitude: Double, longitude: Double): UTMCoord {
-        return UTMCoord.fromLatLon(
+    fun getUtmZone(latitude: Double, longitude: Double): String {
+        val utmCoordinate = getUtmCoordinate(latitude, longitude)
+        val latitudeBand = getUtmLatitudeBand(latitude)
+        return "${utmCoordinate.zone}$latitudeBand "
+    }
+
+    private fun getUtmCoordinate(latitude: Double, longitude: Double): UTMCoord = UTMCoord
+        .fromLatLon(
             Angle.fromDegreesLatitude(latitude),
             Angle.fromDegreesLongitude(longitude)
         )
-    }
 
     fun getCoordinates(
         latitude: Double,
         longitude: Double,
         coordinatesFormat: CoordinatesFormat
-    ): String {
-        return when (coordinatesFormat) {
-            DECIMAL -> {
-                val latitudeText = getDecimalLatitude(latitude)
-                val longitudeText = getDecimalLongitude(longitude)
-                "$latitudeText, $longitudeText"
-            }
-            DMS -> {
-                val latitudeText = getDmsLatitude(latitude)
-                val longitudeText = getDmsLongitude(longitude)
-                "$latitudeText, $longitudeText"
-            }
-            MGRS -> {
-                getMgrsCoordinates(latitude, longitude)
-            }
-            UTM -> {
-                val zoneText = getUtmZone(latitude, longitude)
-                val eastingText = getUtmEasting(latitude, longitude)
-                val northingText = getUtmNorthing(latitude, longitude)
-                "$zoneText $eastingText $northingText"
-            }
+    ): String = when (coordinatesFormat) {
+        DECIMAL -> {
+            val latitudeText = getDecimalLatitude(latitude)
+            val longitudeText = getDecimalLongitude(longitude)
+            "$latitudeText, $longitudeText"
+        }
+        DMS -> {
+            val latitudeText = getDmsLatitude(latitude)
+            val longitudeText = getDmsLongitude(longitude)
+            "$latitudeText, $longitudeText"
+        }
+        MGRS -> {
+            getMgrsCoordinates(latitude, longitude)
+        }
+        UTM -> {
+            val zoneText = getUtmZone(latitude, longitude)
+            val eastingText = getUtmEasting(latitude, longitude)
+            val northingText = getUtmNorthing(latitude, longitude)
+            "$zoneText $eastingText $northingText"
         }
     }
 
     fun getSpeed(
         location: Location?,
         metric: Boolean
-    ): String = if (location == null || !location.hasSpeed()) {
+    ): String = if (location == null || !location.hasSpeed() || location.speed == 0f) {
         locationValueUnknown
     } else {
         val speed = if (metric)
