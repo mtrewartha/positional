13af80c0dd56bcac904831fb6d0136e92bc7fc6d
Mike Trewartha <trewartha.mike@gmail.com>
2023-07-06 12:29:20 -0500

    Add explicit dependency on AndroidX activity lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
 
     implementation(project(":domain"))
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.activity.ktx)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.windowSizeClass)
