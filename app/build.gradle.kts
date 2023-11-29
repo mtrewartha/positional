3b047413d90f3a4c10694816b5a94a7190d05bdf
Mike Trewartha <trewartha.mike@gmail.com>
2023-11-28 21:49:45 -0600

    Bump to v3.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "io.trewartha.positional"
         minSdk = libs.versions.android.sdk.min.get().toInt()
         targetSdk = libs.versions.android.sdk.target.get().toInt()
-        versionCode = 21030001
-        versionName = "3.0.1"
+        versionCode = 21030002
+        versionName = "3.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["clearPackageData"] = "true"
