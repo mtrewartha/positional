090a2a939baca5be1632d7c63a767bda807790f8
Mike Trewartha <trewartha.mike@gmail.com>
2023-12-28 10:19:24 -0600

    Reformat each solar times repo test into three phases

--- data/src/test/java/io/trewartha/positional/data/sun/LibrarySolarTimesRepositoryTest.kt
+++ data/src/test/java/io/trewartha/positional/data/sun/LibrarySolarTimesRepositoryTest.kt
@@ -36,96 +36,144 @@ class LibrarySolarTimesRepositoryTest {
     @Test
     fun testAstronomicalDawnAtCoordinatesOnDate() {
         TimeZone.setDefault(mitadDelMundoTimeZone)
-        subject.getAstronomicalDawn(mitadDelMundo, winterSolstice2023).shouldBe(LocalTime(4, 53))
+
+        val result = subject.getAstronomicalDawn(mitadDelMundo, winterSolstice2023)
+
+        result.shouldBe(LocalTime(4, 53))
     }
 
     @Test
     fun testNoAstronomicalDawnAtCoordinatesOnDate() {
         TimeZone.setDefault(longyearbyenTimeZone)
-        subject.getAstronomicalDawn(northOfLongyearbyen, winterSolstice2023).shouldBeNull()
+
+        val result = subject.getAstronomicalDawn(northOfLongyearbyen, winterSolstice2023)
+
+        result.shouldBeNull()
     }
 
     @Test
     fun testNauticalDawnAtCoordinatesOnDate() {
         TimeZone.setDefault(mitadDelMundoTimeZone)
-        subject.getNauticalDawn(mitadDelMundo, winterSolstice2023).shouldBe(LocalTime(5, 19))
+
+        val result = subject.getNauticalDawn(mitadDelMundo, winterSolstice2023)
+
+        result.shouldBe(LocalTime(5, 19))
     }
 
     @Test
     fun testNoNauticalDawnAtCoordinatesOnDate() {
         TimeZone.setDefault(longyearbyenTimeZone)
-        subject.getNauticalDawn(northOfLongyearbyen, winterSolstice2023).shouldBeNull()
+
+        val result = subject.getNauticalDawn(northOfLongyearbyen, winterSolstice2023)
+
+        result.shouldBeNull()
     }
 
     @Test
     fun testCivilDawnAtCoordinatesOnDate() {
         TimeZone.setDefault(mitadDelMundoTimeZone)
-        subject.getCivilDawn(mitadDelMundo, winterSolstice2023).shouldBe(LocalTime(5, 46))
+
+        val result = subject.getCivilDawn(mitadDelMundo, winterSolstice2023)
+
+        result.shouldBe(LocalTime(5, 46))
     }
 
     @Test
     fun testNoCivilDawnAtCoordinatesOnDate() {
         TimeZone.setDefault(longyearbyenTimeZone)
-        subject.getCivilDawn(northOfLongyearbyen, winterSolstice2023).shouldBeNull()
+
+        val result = subject.getCivilDawn(northOfLongyearbyen, winterSolstice2023)
+
+        result.shouldBeNull()
     }
 
     @Test
     fun testSunriseAtCoordinatesOnDate() {
         TimeZone.setDefault(mitadDelMundoTimeZone)
-        subject.getSunrise(mitadDelMundo, winterSolstice2023).shouldBe(LocalTime(6, 8))
+
+        val result = subject.getSunrise(mitadDelMundo, winterSolstice2023)
+
+        result.shouldBe(LocalTime(6, 8))
     }
 
     @Test
     fun testNoSunriseAtCoordinatesOnDate() {
         TimeZone.setDefault(longyearbyenTimeZone)
-        subject.getSunrise(northOfLongyearbyen, winterSolstice2023).shouldBeNull()
+
+        val result = subject.getSunrise(northOfLongyearbyen, winterSolstice2023)
+
+        result.shouldBeNull()
     }
 
     @Test
     fun testSunsetAtCoordinatesOnDate() {
         TimeZone.setDefault(mitadDelMundoTimeZone)
-        subject.getSunset(mitadDelMundo, winterSolstice2023).shouldBe(LocalTime(18, 16))
+
+        val result = subject.getSunset(mitadDelMundo, winterSolstice2023)
+
+        result.shouldBe(LocalTime(18, 16))
     }
 
     @Test
     fun testNoSunsetAtCoordinatesOnDate() {
         TimeZone.setDefault(longyearbyenTimeZone)
-        subject.getSunset(northOfLongyearbyen, winterSolstice2023).shouldBeNull()
+
+        val result = subject.getSunset(northOfLongyearbyen, winterSolstice2023)
+
+        result.shouldBeNull()
     }
 
     @Test
     fun testCivilDuskAtCoordinatesOnDate() {
         TimeZone.setDefault(mitadDelMundoTimeZone)
-        subject.getCivilDusk(mitadDelMundo, winterSolstice2023).shouldBe(LocalTime(18, 38))
+
+        val result = subject.getCivilDusk(mitadDelMundo, winterSolstice2023)
+
+        result.shouldBe(LocalTime(18, 38))
     }
 
     @Test
     fun testNoCivilDuskAtCoordinatesOnDate() {
         TimeZone.setDefault(longyearbyenTimeZone)
-        subject.getCivilDusk(northOfLongyearbyen, winterSolstice2023).shouldBeNull()
+
+        val result = subject.getCivilDusk(northOfLongyearbyen, winterSolstice2023)
+
+        result.shouldBeNull()
     }
 
     @Test
     fun testNauticalDuskAtCoordinatesOnDate() {
         TimeZone.setDefault(mitadDelMundoTimeZone)
-        subject.getNauticalDusk(mitadDelMundo, winterSolstice2023).shouldBe(LocalTime(19, 4))
+
+        val result = subject.getNauticalDusk(mitadDelMundo, winterSolstice2023)
+
+        result.shouldBe(LocalTime(19, 4))
     }
 
     @Test
     fun testNoNauticalDuskAtCoordinatesOnDate() {
         TimeZone.setDefault(longyearbyenTimeZone)
-        subject.getNauticalDusk(northOfLongyearbyen, winterSolstice2023).shouldBeNull()
+
+        val result = subject.getNauticalDusk(northOfLongyearbyen, winterSolstice2023)
+
+        result.shouldBeNull()
     }
 
     @Test
     fun testAstronomicalDuskAtCoordinatesOnDate() {
         TimeZone.setDefault(mitadDelMundoTimeZone)
-        subject.getAstronomicalDusk(mitadDelMundo, winterSolstice2023).shouldBe(LocalTime(19, 31))
+
+        val result = subject.getAstronomicalDusk(mitadDelMundo, winterSolstice2023)
+
+        result.shouldBe(LocalTime(19, 31))
     }
 
     @Test
     fun testNoAstronomicalDuskAtCoordinatesOnDate() {
         TimeZone.setDefault(longyearbyenTimeZone)
-        subject.getAstronomicalDusk(northOfLongyearbyen, winterSolstice2023).shouldBeNull()
+
+        val result = subject.getAstronomicalDusk(northOfLongyearbyen, winterSolstice2023)
+
+        result.shouldBeNull()
     }
 }
