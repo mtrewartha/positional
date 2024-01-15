b15768f532694161c540915992ccac007c771591
Mike Trewartha <trewartha.mike@gmail.com>
2024-01-15 13:58:33 -0600

    Mark CompassState as @Immutable

--- app/src/main/kotlin/io/trewartha/positional/ui/compass/CompassState.kt
+++ app/src/main/kotlin/io/trewartha/positional/ui/compass/CompassState.kt
@@ -1,10 +1,12 @@
 package io.trewartha.positional.ui.compass
 
+import androidx.compose.runtime.Immutable
 import io.trewartha.positional.data.compass.Azimuth
 import io.trewartha.positional.data.compass.CompassMode
 import io.trewartha.positional.data.measurement.Angle
 import io.trewartha.positional.data.ui.CompassNorthVibration
 
+@Immutable
 sealed interface CompassState {
 
     data object SensorsMissing : CompassState
